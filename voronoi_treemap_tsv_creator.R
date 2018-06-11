78b1be807222c4e2aab9dfe58ca67bb53c5b205e
Praveen Baskaran <praveenbaskaran@praveens-mbp.am10.uni-tuebingen.de>
2018-06-11 17:14:51 +0200

    updated function to include sanity check

--- voronoi_treemap_tsv_creator.R
+++ voronoi_treemap_tsv_creator.R
@@ -87,6 +87,23 @@ voronoi_treemap_tsv_creator<-function(termfile, datafile,outfile,columnmerge){
   colnames(data)[grep(pattern = columnmerge,colnames(data))]<-"gene"
   output<-merge(termsNew,data,by="gene")  
   colnames(output)[grep(pattern = "gene",colnames(output))]<-columnmerge
+  output<-output[,c(2,1,3:ncol(output))]
+  if (ncol(output) == 3){ ### if only three colums are in the output, add a second column with NAs
+    output$level1<-NA
+    output<-output[,c(1,ncol(output),2,3:(ncol(output)-1))]
+  }
+  ### santiy check ## remove terms/pathways with
+  first_col<-colnames(output)[1]
+  terms_ngenes<-ddply(output,.variables = first_col,summarise,n=nrow(piece))
+  terms_ngenes<-terms_ngenes[terms_ngenes$n >= 2,]
+  
+  
+  ### check for duplicates.
+  terms_ngenes_entry<-ddply(output,.variables = c(first_col,columnmerge),summarise,n= nrow(piece))
+
+  
+  output<-output[output[,1] %in% c(as.vector(terms_ngenes[,1]),as.vector(pathway_ngenes[,1])),]
+  
   
   write.table(x = output,file = outfile,quote = F,sep = "\t",row.names = F,col.names = T)
   message(sprintf("Output saved in :::: %s",outfile))
